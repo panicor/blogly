1542d3431108eabaa9f3f9c87a15b4363e57aa7c
Rachel Panico <rachelpanico@Rachels-Air.home>
2023-04-18 17:14:02 -0400

    second commit

--- test_models.py
+++ test_models.py
@@ -26,26 +26,3 @@ class UserModelTestCase(TestCase):
         """Clean up any fouled transaction."""
 
         db.session.rollback()
-
-
-
-
-    # def test_greet(self):
-    #     pet = Pet(name="TestPet", species="dog", hunger=10)
-    #     self.assertEquals(pet.greet(), "I'm TestPet the dog")
-
-    # def test_feed(self):
-    #     pet = Pet(name="TestPet", species="dog", hunger=10)
-    #     pet.feed(5)
-    #     self.assertEquals(pet.hunger, 5)
-
-    #     pet.feed(999)
-    #     self.assertEquals(pet.hunger, 0)
-
-    # def test_get_by_species(self):
-    #     pet = Pet(name="TestPet", species="dog", hunger=10)
-    #     db.session.add(pet)
-    #     db.session.commit()
-
-    #     dogs = Pet.get_by_species('dog')
-    #     self.assertEquals(dogs, [pet])
